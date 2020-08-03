b5e8055840a8bdfe98f1eb0531f127c25f973242
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-08-03 19:15:01 -0400

    refactoring and arguments checking

--- bot.js
+++ bot.js
@@ -34,16 +34,29 @@ client.on('message', message => {
     // create an args variable that slices off the prefix entirely, removes the leftover whitespaces and then splits it into an array by spaces.
     // args.shift() will take the first element in array and return it while also removing it from the original array 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
 
     // if the command does not exist
-    if (!client.commands.has(command)) {
+    if (!client.commands.has(commandName)) {
         message.reply("I'm sorry, I don't recognize that command :confused:");
         return;
     };
 
+    const command = client.commands.get(commandName);
+
+    // check if the command needed arguments but no arguments were provided
+    // command.usage can be used to inform the user how to execute the command
+    if (command.args && !args.length) {
+        let reply = (`You didn't specify any arguments, ${message.author}!`);
+        if (command.usage) {
+            reply += `\nThe proper usage would be ${prefix}${command.name} ${command.usage}`;
+        }
+
+        return message.channel.send(reply);
+    }
+
     try {
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
--- commands/args.js
+++ commands/args.js
@@ -1,11 +1,9 @@
 module.exports = {
     name: 'args-info',
     description: 'Testing commands and arguments',
+    args: true,
+    usage: '<args>',
     execute(message, args) {
-        if (!args.length) {
-            return message.channel.send(`You didn't specify any arguments, ${message.author}!`);
-        }
-
         message.channel.send(`Command name: args-info\nArguments: ${args}`);
     }
 }
